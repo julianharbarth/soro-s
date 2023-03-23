fccb782ae54fff837d24b5ef6097bac0e97b9303
julianharbarth <julian@harbarth.io>
2023-03-23 15:21:01 +0100

    sanitizer fixes

--- web/server/src/modules/tiles/tiles_module.cc
+++ web/server/src/modules/tiles/tiles_module.cc
@@ -50,8 +50,10 @@ net::web_server::string_res_t tiles_module::serve_tile(
 
 fs::path create_osm_file(server_settings const& settings,
                          soro::infra::infrastructure const& infra) {
+
   auto osm_path =
-      (settings.osm_dir() / infra->source_.data()).replace_extension(".osm");
+      (settings.osm_dir() / static_cast<std::string_view>(infra->source_))
+          .replace_extension(".osm");
 
   if (settings.regenerate_.val() || !fs::exists(osm_path)) {
     osm_export::export_and_write(*infra, osm_path);
@@ -64,7 +66,8 @@ fs::path create_tiles_db(server_settings const& s,
                          soro::infra::infrastructure const& infra,
                          fs::path const& osm_path) {
   auto tile_db_path =
-      (s.tiles_dir() / infra->source_.data()).replace_extension(".mdb");
+      (s.tiles_dir() / static_cast<std::string_view>(infra->source_))
+          .replace_extension(".mdb");
 
   if (s.regenerate_.val() || !fs::exists(tile_db_path) ||
       last_write_time(osm_path) > last_write_time(tile_db_path)) {
@@ -87,7 +90,7 @@ tiles_module get_tile_module(server_settings const& settings,
     auto const tile_db_path = create_tiles_db(settings, infra, osm_path);
 
     auto [_, success] =
-        result.tile_contexts_.try_emplace(infra->source_.data(), tile_db_path);
+        result.tile_contexts_.try_emplace(infra->source_, tile_db_path);
 
     utls::sassert(success,
                   "naming conflict during tile db creation with name {}",
